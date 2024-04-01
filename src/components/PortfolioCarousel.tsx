ba3844dc8be0370c93f68ed55b3f09f330777560
archieQa <luarsarsi@gmail.com>
2024-04-01 15:43:48 +0200

    done with the portfolio for now

--- src/components/PortfolioCarousel.tsx
+++ src/components/PortfolioCarousel.tsx
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import "@/styles/globals.css";
 
 const imageList = [
-  "/pic1.png",
-  "/pic2.png",
-  "/pic3.png",
-  // Ensure all paths are valid and complete your image list
+  { src: "/pic1.png", name: "Image Name 1" },
+  { src: "/pic2.png", name: "Image Name 2" },
+  { src: "/pic3.png", name: "Image Name 3" },
+  // Continue with your images
 ];
-
 function PortfolioCarousel() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const currentImage = imageList[currentIndex]; // Access the current image object
 
   const goToPrevious = () => {
     const newIndex =
@@ -25,23 +25,21 @@ function PortfolioCarousel() {
 
   return (
     <div className="flex flex-col justify-items-start">
-      <div className="absolute left-[100px] translate-y-48 ">
-        {" "}
+      <div className="absolute left-[200px] translate-y-48 ">
         <button
           onClick={goToPrevious}
-          className=" text-black border-none cursor-pointer z-10 text-8xl" // Larger arrows
+          className="text-black border-none cursor-pointer z-10 text-8xl" // Larger arrows
         >
           ‹
         </button>
       </div>
       <div className="transform -translate-y-28 translate-x-3">
         <img
-          src={imageList[currentIndex]}
-          className="w-[195px] h-[244px] sm:w-[250px] sm:h-[300px] shadow-2xl"
+          src={currentImage.src} // Use src from currentImage
+          className="w-[195px] h-[244px] sm:w-[250px] sm:h-[300px] shadow-bottom-right"
         />
       </div>
-      <div className="absolute right-[100px] translate-y-48">
-        {" "}
+      <div className="absolute right-[200px] translate-y-48">
         <button
           onClick={goToNext}
           className="text-black border-none cursor-pointer z-10 text-8xl" // Larger arrows
@@ -49,9 +47,10 @@ function PortfolioCarousel() {
           ›
         </button>
       </div>
-      <div className="-translate-y-28 translate-x-3 py-5">
-        <h1 className="shadow-text pt-10 text-center text-black text-2xl ">
-          Name of the Painting
+      <div className="-translate-y-32 translate-x-3 py-5">
+        {/* Use name from currentImage */}
+        <h1 className="shadow-text pt-10 text-center text-black text-xl ">
+          {currentImage.name}
         </h1>
       </div>
     </div>
