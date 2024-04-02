79da78c1fff684c028f262f6256c85fd611b8add
kenidervishaj <krenar.dervishaj@fau.de>
2024-04-02 15:31:47 +0200

    Prints fully responsive

--- src/components/Paintings.tsx
+++ src/components/Paintings.tsx
@@ -7,16 +7,12 @@ export type Painting = {
   description: string;
   image: string;
   price: string;
-  imageClassName: string;
-  divClassPrice: string;
 };
 
 export const paintings: Painting[] = [
   {
     paintingname: "Into the Walls",
     image: "/thewalls.png",
-    imageClassName: "w-[200px] h-[200px]",
-    divClassPrice: "flex flex-row space-x-[95px] mt-24",
     id: 1,
     artist: "Joan Popli",
     description: "Oil on canvas, 2020",
@@ -25,8 +21,6 @@ export const paintings: Painting[] = [
   {
     paintingname: "Wallowing Breeze",
     image: "/wallowing.png",
-    imageClassName: "w-[200px] h-[230px] mt-[-15px]",
-    divClassPrice: "flex flex-row space-x-[95px] mt-20",
     id: 3,
     artist: "Joan Popli",
     description: "Oil on canvas, 2008",
@@ -35,8 +29,6 @@ export const paintings: Painting[] = [
   {
     paintingname: "J Resistance",
     image: "/resistance.png",
-    imageClassName: "w-[230px] h-[200px]",
-    divClassPrice: "flex flex-row space-x-[125px] mt-24",
     id: 4,
     artist: "Joan Popli",
     description: "Gouache on paper, 2019",
@@ -45,8 +37,6 @@ export const paintings: Painting[] = [
   {
     paintingname: "Warm Basket",
     image: "/basket.png",
-    imageClassName: "w-[200px] h-[230px] mt-[-15px]",
-    divClassPrice: "flex flex-row space-x-[95px] mt-20",
     id: 5,
     artist: "Joan Popli",
     description: "Acrylic on wood, 2014",
@@ -55,8 +45,6 @@ export const paintings: Painting[] = [
   {
     paintingname: "The Vonnegut",
     image: "/vonnegut.png",
-    imageClassName: "w-[200px] h-[200px]",
-    divClassPrice: "flex flex-row space-x-[100px] mt-24",
     id: 6,
     artist: "Joan Popli",
     description: "Oil on canvas, 2014",
@@ -65,8 +53,6 @@ export const paintings: Painting[] = [
   {
     paintingname: "The Vonnegut",
     image: "/vonnegut.png",
-    imageClassName: "w-[220px] h-[220px] mt-[-15px] ",
-    divClassPrice: "flex flex-row space-x-[115px] mt-24",
     id: 7,
     artist: "Joan Popli",
     description: "Oil on canvas, 2014",
@@ -75,8 +61,6 @@ export const paintings: Painting[] = [
   {
     paintingname: "The Vonnegut",
     image: "/vonnegut.png",
-    imageClassName: "w-[200px] h-[200px]",
-    divClassPrice: "flex flex-row space-x-[100px] mt-24",
     id: 8,
     artist: "Joan Popli",
     description: "Oil on canvas, 2014",
@@ -85,8 +69,6 @@ export const paintings: Painting[] = [
   {
     paintingname: "The Vonnegut",
     image: "/vonnegut.png",
-    imageClassName: "w-[220px] h-[220px] mt-[-15px] ",
-    divClassPrice: "flex flex-row space-x-[115px] mt-24",
     id: 9,
     artist: "Joan Popli",
     description: "Oil on canvas, 2014",
@@ -95,8 +77,6 @@ export const paintings: Painting[] = [
   {
     paintingname: "The Vonnegut",
     image: "/vonnegut.png",
-    imageClassName: "w-[200px] h-[200px]",
-    divClassPrice: "flex flex-row space-x-[100px] mt-24",
     id: 10,
     artist: "Joan Popli",
     description: "Oil on canvas, 2014",
