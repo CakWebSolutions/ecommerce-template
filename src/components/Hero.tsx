0b5643dc827bfb697771659f02374686c2006c1e
CakWebSolutions <info@cakwebsolutions.com>
2024-04-02 14:02:58 +0200

    navbar hover color

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,14 +12,14 @@ function Hero2() {
     return (
         <div className="">
             <div className='flex flex-col justify-between lg:justify-between bg-white h-screen'>
-                <div className='flex flex-col z-50 text-center text-black items-center mt-20 md:mt-10 lg:mt-5 space-y-2'>
+                <div className='flex flex-col z-50 text-center text-black items-center mt-16 md:mt-10 lg:mt-5 space-y-2'>
                     <h1 className='text-6xl font-playfairBold opacity-75 font-bold'>Joan</h1>
                     <h1 className='text-6xl font-playfairBold opacity-70 font-bold'>Popli`s</h1>
                     <h1 className='text-7xl font-playfairRegular opacity-60'>Gallery</h1>
                     <div className="group inline-block">
                         <Link legacyBehavior href='/AboutMe'>
                             <a>
-                                <button className='bg-[#c4c0c0] transition-opacity duration-300 rounded-md p-1 px-8 opacity-60 hover:opacity-100 mt-8 relative overflow-hidden'>
+                                <button className='bg-[#c4c0c0] transition-opacity duration-300 rounded-md p-1 px-8 opacity-100 lg:opacity-60 hover:opacity-100 mt-8 relative overflow-hidden'>
                                     Get in Contact
                                     <div className="hidden absolute top-0 left-0 w-full h-full bg-gradient-to-t from-transparent to-bg-[#c4c0c0] group-hover:translate-x-full transition-transform duration-500"></div>
                                 </button>
@@ -30,7 +30,7 @@ function Hero2() {
 
                 <div className='lg:hidden overflow-y-hidden flex flex-col w-screen space-x-4 items-end -mt-56 relative' style={{ zIndex: 10 }}>
                     {['/pic4.png'].map((pic, i) => (
-                        <div key={i} className="group max-h-screen relative w-auto hover:opacity-100 flex items-end">
+                        <div key={i} className="group max-h-screen pt-2 relative w-auto hover:opacity-100 flex items-end">
                             <img src={pic} alt='paintings' className={`max-h-full ${boxShadow}`} />
                             {/* Adjust opacity on hover within the group */}
                             <div className="absolute inset-0 bg-gradient-to-t from-transparent to-white transition-opacity duration-500"></div>
